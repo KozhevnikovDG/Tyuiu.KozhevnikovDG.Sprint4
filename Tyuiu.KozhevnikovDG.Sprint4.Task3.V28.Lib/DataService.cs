05a06c272c2d05e073135ad72a1a7a8d10f94741
Tyuiu.KozhevnikovDG@outlook.com <f1zog2005@mail.ru>
2023-11-04 23:54:02 +0500

    Task3

---
+++ Tyuiu.KozhevnikovDG.Sprint4.Task3.V28.Lib/DataService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.KozhevnikovDG.Sprint4.Task3.V28.Lib
+{
+    public class DataService : ISprint4Task3V28
+    {
+        public int Calculate(int[,] array)
+        {
+            int rows = array.GetUpperBound(0) + 1;
+            int colums = array.Length / rows;
+            
+            int minElement = array[0, 3];
+
+            for (int j = 0; j < colums; j++)
+            {
+                for(int i = 0; i < rows; i++)
+                {
+                    if(array[i,3] < minElement)
+                    {
+                        minElement = array[i, 3];
+                    }
+                }
+            }
+            return minElement;
+        }
+    }
+}
---
+++ Tyuiu.KozhevnikovDG.Sprint4.Task3.V28/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KozhevnikovDG.Sprint4.Task3.V28.Lib;
+namespace Tyuiu.KozhevnikovDG.Sprint4.Task3.V28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #4 | Выполнил: Кожевников Д. Г. | ИИПБ-23-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Обработка структурных типов                                       *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #28                                                             *");
+            Console.WriteLine("* Выполнил: Кожевников Данил Георгиевич | ИИПБ-23-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* ан двумерный целочисленный массив 5 на 5 элементов,заполненный          *");
+            Console.WriteLine("*статическими значениями в диапазоне от 4 до 9.Найдите минимальный элемент*");
+            Console.WriteLine("* в четвертом столбце                                                     *");
+
+            DataService ds = new DataService();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int[,] mtrx = new int[5, 5] { { 5, 4, 9, 8, 5, },
+                                          { 4, 6, 6, 9, 9, },
+                                          { 9, 8, 4, 8, 8, },
+                                          { 6, 8, 6, 8, 9, },
+                                          { 9, 4, 6, 6, 7, } };
+            int rows = mtrx.GetUpperBound(0) + 1;
+            int colums = mtrx.Length / rows;
+
+            Console.WriteLine("Массив");
+            for (int i = 0; i < rows;i++)
+            {
+                for(int j = 0; j < colums;j++)
+                {
+                    Console.WriteLine($"{mtrx[i,j]} \t");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            int res = ds.Calculate(mtrx);
+
+            Console.WriteLine("Минимальный элемент в четвертом столбце = " + res);
+            Console.ReadKey();
+        }
+    }
+}
